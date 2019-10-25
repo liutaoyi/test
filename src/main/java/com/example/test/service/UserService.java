d671250669611c4b77282ae527e70a6d6156cf76
liutaoyi <2580295193@qq.com>
2019-10-25 11:00:23 +0800

    remove unused functions

--- src/main/java/com/example/test/service/UserService.java
+++ src/main/java/com/example/test/service/UserService.java
@@ -1,19 +1,15 @@
 package com.example.test.service;
 
-import com.example.test.dto.MyDTO;
-import com.example.test.dto.Page;
-import com.example.test.entity.User;
-
 import java.util.Date;
 import java.util.List;
 
 public interface UserService {
-    void insert(User user);
-    List<User> query();
+    //    void insert(User user);
+//    List<User> query();
 //    LoginDto loginForApp();
-    User login(String username, String password);
-    Page queryAll(Integer page,Integer size);
-    int test(MyDTO myDTO);
-    void delete();
+//    User login(String username, String password);
+//    Page queryAll(Integer page,Integer size);
+//    int test(MyDTO myDTO);
+//    void delete();
     List getByCondition(Date time);
 }
--- src/main/java/com/example/test/service/UserServiceImpl.java
+++ src/main/java/com/example/test/service/UserServiceImpl.java
@@ -1,18 +1,12 @@
 package com.example.test.service;
 
-import com.example.test.dto.LoginDto;
-import com.example.test.dto.MyDTO;
-import com.example.test.dto.Page;
-import com.example.test.entity.User;
 import com.example.test.entity.UserExample;
 import com.example.test.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
-import java.util.TimeZone;
 
 /**
  * @author: LiuTaoYi
@@ -24,60 +18,6 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     UserMapper userMapper;
-
-    @Override
-    public void insert(User user) {
-        userMapper.insert(user);
-    }
-
-    @Override
-    public List<User> query() {
-        UserExample userExample = new UserExample();
-        Calendar c = Calendar.getInstance();
-        c.setTime(new Date());
-        c.add(Calendar.MONTH, -1);
-        Date m = c.getTime();
-        userExample.createCriteria().andBirthGreaterThan(m);
-        return userMapper.selectByExample(userExample);
-    }
-
-//    @Override
-//    public LoginDto loginForApp() {
-//        LoginDto loginDto = userMapper.getLoginResponse();
-//        return loginDto;
-//    }
-//
-    @Override
-    public User login(String username, String password) {
-        return userMapper.login(username,password);
-    }
-
-    @Override
-    public Page queryAll(Integer page, Integer size) {
-        Page<User> pageInfo = new Page<>(size,page,query());
-        return pageInfo;
-    }
-
-    @Override
-    public int test(MyDTO myDTO) {
-        if (myDTO.getTime() == null){
-            TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
-            myDTO.setTime(new Date());
-        }
-        System.out.println(myDTO.getTime());
-        User user = new User();
-        user.setUsername(myDTO.getUsername());
-        user.setPassword(myDTO.getPassword());
-        user.setBirth(myDTO.getTime());
-        return userMapper.insertSelective(user);
-    }
-
-    @Override
-    public void delete() {
-        UserExample example = new UserExample();
-        example.createCriteria().andPasswordEqualTo("123456");
-        userMapper.deleteByExample(example);
-    }
 
     @Override
     public List getByCondition(Date time) {
