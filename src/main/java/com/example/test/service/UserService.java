ffab0f076eacdddf45498c908173df942784d86f
liutaoyi <2580295193@qq.com>
2019-10-25 11:06:25 +0800

    reformat

--- src/main/java/com/example/test/service/UserService.java
+++ src/main/java/com/example/test/service/UserService.java
@@ -4,12 +4,6 @@ import java.util.Date;
 import java.util.List;
 
 public interface UserService {
-    //    void insert(User user);
-//    List<User> query();
-//    LoginDto loginForApp();
-//    User login(String username, String password);
-//    Page queryAll(Integer page,Integer size);
-//    int test(MyDTO myDTO);
-//    void delete();
+
     List getByCondition(Date time);
 }
