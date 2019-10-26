01e2415a99b9beca41f837b313c258cbcb01f471
liutaoyi <2580295193@qq.com>
2019-10-26 14:41:22 +0800

    reformat

--- src/main/java/com/example/test/controller/UserController.java
+++ src/main/java/com/example/test/controller/UserController.java
@@ -1,16 +1,15 @@
 package com.example.test.controller;
 
-import com.example.test.dto.MyDTO;
-import com.example.test.entity.User;
 import com.example.test.service.UserService;
 import com.example.test.utils.Result;
 import com.example.test.utils.ResultUtil;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
 
