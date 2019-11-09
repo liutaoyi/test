7593f58e40aa3c4c0791466aab014e373b2dc317
liutaoyi <2580295193@qq.com>
2019-11-09 23:47:47 +0800

    配置mybatis-plus 学习mybatis-plus

--- src/main/java/com/example/test/service/SampleTest.java
+++ src/main/java/com/example/test/service/SampleTest.java
@@ -1,5 +1,6 @@
 package com.example.test.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.example.test.entity.MyUser;
 import com.example.test.mapper.MyUserMapper;
 import org.junit.Assert;
@@ -28,5 +29,36 @@ public class SampleTest {
         List<MyUser> userList = myUserMapper.selectList(null);
         Assert.assertEquals(5, userList.size());
         userList.forEach(System.out::println);
+    }
+
+    @Test
+    public void testInsert() {
+        MyUser myUser = new MyUser();
+        myUser.setName("小小小马云");
+        myUser.setAge(1);
+        myUser.setEmail("336699@qq.com");
+        int rows = myUserMapper.insert(myUser);
+        Assert.assertEquals(1, rows);
+        System.out.println("影响记录数: " + rows);
+    }
+
+    @Test
+    public void testRetrieve() {
+//        MyUser myUser = myUserMapper.selectById(1L);
+//        System.out.println(myUser);
+
+//        List<Long> list = Arrays.asList(1L, 2L, 3L);
+//        List<MyUser> myUsers = myUserMapper.selectBatchIds(list);
+//        myUsers.forEach(System.out::println);
+
+//        Map<String,Object> map = new HashMap<>();
+//        map.put("name","Jone");
+//        List<MyUser> myUsers1 = myUserMapper.selectByMap(map);
+//        myUsers1.forEach(System.out::println);
+
+        QueryWrapper<MyUser> queryWrapper = new QueryWrapper<>();
+        queryWrapper.like("name", "").lt("age", 100);
+        List<MyUser> myUsers = myUserMapper.selectList(queryWrapper);
+        myUsers.forEach(System.out::println);
     }
 }
