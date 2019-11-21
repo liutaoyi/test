b72e34e9b1f66435d9ae373557d6b7d914847eff
liutaoyi <2580295193@qq.com>
2019-11-21 22:33:08 +0800

    Java8

---
+++ src/main/java/com/example/test/utils/Java8NewFeaturesTest.java
@@ -0,0 +1,13 @@
+package com.example.test.utils;
+
+/**
+ * @author: LiuTaoYi
+ * @create_time: 2019-11-21 22:31
+ * @description:
+ **/
+
+public class Java8NewFeaturesTest {
+    public static void main(String[] args) {
+        
+    }
+}
