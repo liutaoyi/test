a7dd925ac070f4b4edb9b0243f48a6ac9a4b9557
liutaoyi <2580295193@qq.com>
2019-11-22 10:10:34 +0800

    整理

---
+++ src/main/java/com/example/test/controller/Test1Controller.java
@@ -0,0 +1,16 @@
+package com.example.test.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: LiuTaoYi
+ * @create_time: 2019-11-22 10:08
+ * @description:
+ **/
+@RestController
+@Api(value = "测试1")
+@RequestMapping(value = "/cao")
+public class Test1Controller {
+}
