7782297e078ee824d9cdfc70968b2f40435dfb7d
liutaoyi <2580295193@qq.com>
2019-11-24 21:17:23 +0800

    upload

---
+++ src/main/java/com/example/test/controller/FileUploadController.java
@@ -0,0 +1,48 @@
+package com.example.test.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author: LiuTaoYi
+ * @create_time: 2019-11-24 20:59
+ * @description:
+ **/
+@RestController
+@Api(value = "文件上传")
+@RequestMapping(value = "/upload")
+public class FileUploadController {
+
+    public String upload(@RequestParam("file") MultipartFile file) {
+        try {
+            if (file.isEmpty()) {
+                return "文件为空";
+            }
+            // 获取文件名
+            String fileName = file.getOriginalFilename();
+            // 获取文件的后缀名
+//            String suffixName = fileName.substring(fileName.lastIndexOf("."));
+            // 设置文件存储路径
+            String filePath = "C:/Users/Administrator/Desktop/upload";
+            String path = filePath + fileName;
+            File dest = new File(path);
+            // 检测是否存在目录
+            if (!dest.getParentFile().exists()) {
+                dest.getParentFile().mkdirs();// 新建文件夹
+            }
+            file.transferTo(dest);// 文件写入
+            return "上传成功";
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "上传失败";
+    }
+}
